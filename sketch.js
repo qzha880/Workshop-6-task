13c542488ef4041b1db1d70d6ae9a7ca2c2d4680
qzha880 <qzha880@aucklanduni.ac.nz>
2025-01-27 21:02:07 +0800

    Add files via upload
    Updated the sketch file

--- sketch.js
+++ sketch.js
@@ -1,40 +1,38 @@
-let userinputtext;
 let helpbutton = [];
 let textoptions = [];
-let redslider;
-let greenslider;
-let blueslider;
+let h1, h2, h3;
+
+let userInput;
+let button;
+let userLine;
+
+let poem  = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  userinputtext = createInput();
-  userinputtext.position(53,90);
+  userInput = createInput();
+  userInput.position(40, 90);
+  button  = createButton('write poem');
+  button.position(userInput.x, userInput.y + 21);
+  button.mousePressed(newLine);
+  
+  let h1 = RiTa.phones("Help");
+  let h2 = RiTa.phones("I don\'t know what to do");
+  let h3 = RiTa.phones("Brain not working");
+  textoptions.push("Help");
+  textoptions.push("I don\'t know what to do");
+  textoptions.push("Brain not working");
+  textoptions.push(h1);
+  textoptions.push(h2);
+  textoptions.push(h3);
 
-  textWrap(CHAR);
-
-  textoptions.push('Help');
-  textoptions.push('I don\'t know what to do');
-  textoptions.push('Brain not working');
-
-  let button = createButton('Help');
-  button.position(263, 90);
-  button.mousePressed(() => {
+  let button2 = createButton('Help');
+  button2.position(270 , 90);
+  button2.mousePressed(() => {
     let r = random(textoptions);
     helpbutton.push(r);
   });
-
-  redslider = createSlider(0,255);
-  redslider.position(50, 3);
-  redslider.size(255);
-
-  greenslider = createSlider(0,255);
-  greenslider.position(50, 37);
-  greenslider.size(255);
-
-  blueslider = createSlider(0,255);
-  blueslider.position(50, 70);
-  blueslider.size(255);
 
 }
 
@@ -42,19 +40,55 @@ function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
 }
 
-// function mousePressed(){
-//   text('Help', 240, 150);
-// }
-
 function draw() {
-  let red = redslider.value();
-  let blue = blueslider.value();
-  let green = greenslider.value();
-  background(red,green,blue);
-  let usertext = userinputtext.value();
-  text(usertext, 53, 150, 185);
-  
+  background(80);
+  writePoem();
+
   for (i=0;i<helpbutton.length;i++){
-    text(helpbutton[i],240,150 + i * 20);
+    fill(255, 0, 0);
+    text(helpbutton[i],240,180 + i * 20);
+  }
+
+  fill(255);
+  text("type in your poem and click 'write poem'", 50, 600);
+  text("your words will be changed", 50, 620);
+  text("(the verb will changed into present tense)", 50, 640);
+  text("(random words may be changed into adverbs)", 50, 660);
+  text("click 'Help' to hear Garuinja scream", 50, 680);
+}
+
+function writePoem(){
+  for(x = 0; x < poem.length; x++){
+    fill(255)
+    text(poem[x], 40, 180 + x * 20);
+  }
+}
+
+function newLine () {
+  userLine = userInput.value();
+  userInput.value('');
+  poem.push(userLine);
+
+  let words = RiTa.tokenize(userLine);
+
+  for (a = 0; a < words.length-1; a++) {
+    if (RiTa.isAdverb(words[a])) {
+      let adverb = RiTa.randomWord({pos: "rb"});
+      words[a] = adverb;
+
+      let secondLine = RiTa.untokenize(words);
+      poem.push(secondLine);
+    }
+  }
+
+  let words1 = RiTa.tokenize(userLine);
+    for (i = 0; i < words1.length-1; i++) {
+    if (RiTa.isVerb(words1[i])) {
+      let present = RiTa.presentPart(words1[i]);
+      words1[i] = present;
+
+      let thirdLine = RiTa.untokenize(words1);
+      poem.push(thirdLine);
+    }
   }
 }
